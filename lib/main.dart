089713c0253bb916329a1a6febf042d673a3697d
Adham167 <adhamabdelsalam3040@gmail.com>
2026-02-01 20:11:54 +0200

    adding ptofile image and name and title

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,49 @@ class UserInfoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
       appBar: AppBar(
+        backgroundColor: Color(0xFFFFFFFF),
         actions: [Icon(Icons.more_vert, color: Color(0xFFDBDBDB))],
       ),
-      body: Center(child: Column(children: [
-            
+      body: Center(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/ln.jpg'),
+                  radius: 60,
+                ),
+                Positioned(
+                  bottom: 10,
+                  right: 0,
+                  child: CircleAvatar(
+                    radius: 12,
+                    backgroundColor: Color(0xFF64C288),
+                    child: Icon(Icons.check, color: Colors.white, size: 15),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 32),
+            Text(
+              "Adham Abdelsalam",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(height: 12),
+            Text(
+              "Flutter Developer",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                color: Color(0xFFD3D3D3),
+              ),
+            ),
           ],
-        )),
+        ),
+      ),
     );
   }
 }
